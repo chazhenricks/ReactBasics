e5f531f8828f0bdbb97d6fdb213e2afc2f8d98c4
Chaz Henricks <chazhenricks@gmail.com>
2017-09-26 10:53:02 -0500

    added ticking functionality to stopwatch using componentDidMount and componentWillUnmount

--- app.jsx
+++ app.jsx
@@ -23,11 +23,34 @@ var Stopwatch = React.createClass({
 getInitialState : function(){
   return {
     runnning: false,
+    elapsedTime: 0,
+    previousTime: 0,
+  }
+},
+
+componentDidMount : function(){
+  this.interval = setInterval(this.onTick, 100);
+},
+
+componentWillUnmount : function(){
+  clearInterval(this.interval);
+},
+
+onTick : function(){
+  if(this.state.running){
+    var now = Date.now();
+    this.setState({
+      previousTime : now,
+      elapsedTime: this.state.elapsedTime + (now - this.state.previousTime),
+    });
   }
 },
 
 onStart : function(){
-  this.setState({running: true});
+  this.setState({
+      running: true,
+      previousTime: Date.now()
+    });
 },
 
 onStop: function(){
@@ -35,15 +58,18 @@ onStop: function(){
 },
 
 reset : function(){
-
+  this.setState({
+    elapsedTime: 0,
+    previousTime: Date.now()
+  })
 },
   render: function(){
-    
+    var seconds = Math.floor(this.state.elapsedTime / 1000);    
 
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
-        <div className="stopwatch-time">0</div>
+        <div className="stopwatch-time">{ seconds }</div>
         { this.state.running ? <button onClick={this.onStop}>Stop</button> : <button onClick={this.onStart}>Start</button> }
         <button onClick={this.reset}>Reset</button>
       </div>
